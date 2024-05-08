d580e619064ed8928e37670e414bc4511c7ef102
Daffodi <j1225623445@gmail.com>
2024-05-08 12:03:55 +0800

    update scrollview

--- src/components/LearningScrollView.vue
+++ src/components/LearningScrollView.vue
@@ -1,37 +1,44 @@
+<!--README:app learning scroll view-->
 <script setup>
 import LearningChapter from "@/components/LearningChapter.vue";
+import LearningSectionTitle from "@/components/LearningSectionTitle.vue";
 </script>
 
 <template>
   <div class="learningScrollViewParent">
-
+    <LearningSectionTitle
+        :number="1" name="Gateway to WEB 3.0"
+    />
     <LearningChapter
-        :finish = "false" :answered-number = "1" :total-number = "15" title = "Centralized Exchange"
+        :finish = "false" :answered-number = "1" :total-number = "15" title = "What is bitcoin?"
         summary="Description Bitcoin is a decentralized digital currency that was created in 2009 by an anonymous"
         picUrl="/learningSection_centralizeExchange.svg"
     />
     <LearningChapter
-        :finish = "false" :answered-number = "1" :total-number = "15" title = "Centralized Exchange"
+        :finish = "false" :answered-number = "0" :total-number = "15" title = "Blockchain technology"
         summary="Description Bitcoin is a decentralized digital currency that was created in 2009 by an anonymous"
         picUrl="/learningSection_centralizeExchange.svg"
     />
     <LearningChapter
-        :finish = "false" :answered-number = "1" :total-number = "15" title = "Centralized Exchange"
+        :finish = "false" :answered-number = "0" :total-number = "15" title = "Gateway into WEB 3 World"
+        summary="Description Bitcoin is a decentralized digital currency that was created in 2009 by an anonymous"
+        picUrl="/learningSection_centralizeExchange.svg"
+    />
+    <LearningSectionTitle
+        :number="2" name="Platform"
+    />
+    <LearningChapter
+        :finish = "false" :answered-number = "0" :total-number = "15" title = "Centralized Exchange"
         summary="Description Bitcoin is a decentralized digital currency that was created in 2009 by an anonymous"
         picUrl="/learningSection_centralizeExchange.svg"
     />
     <LearningChapter
-        :finish = "false" :answered-number = "1" :total-number = "15" title = "Centralized Exchange"
+        :finish = "false" :answered-number = "0" :total-number = "15" title = "Decentralized Exchange"
         summary="Description Bitcoin is a decentralized digital currency that was created in 2009 by an anonymous"
         picUrl="/learningSection_centralizeExchange.svg"
     />
     <LearningChapter
-        :finish = "false" :answered-number = "1" :total-number = "15" title = "Centralized Exchange"
-        summary="Description Bitcoin is a decentralized digital currency that was created in 2009 by an anonymous"
-        picUrl="/learningSection_centralizeExchange.svg"
-    />
-    <LearningChapter
-        :finish = "false" :answered-number = "1" :total-number = "15" title = "Centralized Exchange"
+        :finish = "false" :answered-number = "0" :total-number = "15" title = "Infersturcture"
         summary="Description Bitcoin is a decentralized digital currency that was created in 2009 by an anonymous"
         picUrl="/learningSection_centralizeExchange.svg"
     />
@@ -51,7 +58,7 @@ import LearningChapter from "@/components/LearningChapter.vue";
   padding: 0 1.4rem;
   overflow-y: auto; /* 垂直滚动 */
   /*定位背景*/
-  background: rgba(0, 100, 100, 0.5);
+  /*background: rgba(0, 100, 100, 0.5);*/
 }
 /* 添加垂直间隔 */
 .learningScrollViewParent > * {
---
+++ src/components/LearningSectionTitle.vue
@@ -0,0 +1,54 @@
+<!--README:app learning section title-->
+<script setup>
+defineProps({
+  // 这是第几section
+  number: {
+    type: Number,
+    default: false
+  },
+  // 标题名称
+  name: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="learningSectionTitleParent">
+    <div class="learningSectionTitleName">Level {{number}}: {{name}}</div>
+    <div class="learningSectionTitleLine"/>
+  </div>
+</template>
+
+<style scoped>
+.learningSectionTitleParent {
+  width: 100%;
+  position: relative;
+  height: 31px;
+  text-align: left;
+  font-size: 20px;
+  font-family: Fredoka-Medium;
+  padding: 0 0;
+  /*定位*/
+  /*background: rgba(0, 255, 255, 0.2);*/
+}
+.learningSectionTitleName {
+  position: absolute;
+  height: 100%;
+  width: 70%;
+  background: linear-gradient(-90deg, #ff62c5, #00ecb7 50.5%, #a100ff);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.learningSectionTitleLine {
+  position: absolute;
+  top: 100%;
+  width: 100%;
+  height: 2px;
+  border-radius: 1000px;
+  background: linear-gradient(-90deg, #ff62c5, #00ecb7 50.5%, #a100ff);
+}
+
+
+</style>
